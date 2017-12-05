757187bf7ff7911d81e3b1d583cc43e11305b571
Sergey Kovbasyuk <sergey.kovbasyuk@onereach.com>
2017-12-05 20:50:56 +0200

    added pug and basic styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,34 @@
 const gulp = require('gulp')
+const browsersync = require('browser-sync')
+const pug = require('gulp-pug')
 const sass = require('gulp-sass')
 const autoprefixer = require('gulp-autoprefixer')
+const imagemin = require('gulp-imagemin')
+const pngquant = require('imagemin-pngquant')
 
-gulp.task('img', () => gulp.src('./src/img/**/*.*')
-    .pipe(gulp.dest('./dist/img/')))
+const reload = browsersync.reload
+
+gulp.task('imagemin', () => (
+    gulp.src('./src/img/**/*.{gif,jpg,png}')
+      .pipe(imagemin({
+        progressive: true,
+        interlaced: true,
+        use: [pngquant()],
+      }))
+      .pipe(gulp.dest('./dist/img'))
+  ))
+
+gulp.task('fonts', () => gulp.src('src/fonts/**/*')
+    .pipe(gulp.dest('dist/fonts')))
+
+gulp.task('pug',() =>
+    gulp.src('./src/pug/*.pug')
+        .pipe(pug({
+            pretty:true
+        }))
+        .pipe(gulp.dest('./dist'))
+        .pipe(reload({ stream: true }))
+)
 
 gulp.task('sass', () => gulp.src('./src/styles/main.scss')
     .pipe(sass({
@@ -14,10 +39,22 @@ gulp.task('sass', () => gulp.src('./src/styles/main.scss')
         versions:['last 2 browsers']
     }))
     .pipe(gulp.dest('./dist/'))
-    )
+    .pipe(reload({ stream: true })))
+
+gulp.task('browsersync', () => (
+    browsersync({
+        server: {
+        baseDir: './dist',
+        },
+        open: false,
+    })
+))
 
 gulp.task('watch', () => {
-    gulp.watch('./src/styles/**/*.scss', ['sass']);
-});
+    gulp.watch('./src/styles/**/*.scss', ['sass'])
+    gulp.watch('./src/img/**/*.{gif,jpg,png}', ['imagemin'])
+    gulp.watch('./src/pug/**/*.pug', ['pug'])
+    gulp.watch('./src/fonts/**/*.*', ['fonts'])
+})
 
-gulp.task('default', ['sass','img', 'watch'])
+gulp.task('default', ['fonts', 'imagemin', 'sass', 'pug', 'browsersync', 'watch'])
